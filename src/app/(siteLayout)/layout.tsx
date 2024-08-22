3dfcef5f28a5d3aeb5d6f33b71ea5499302d394c
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-22 16:38:08 -0400

    configuracion base del site layout

---
+++ src/app/(siteLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SiteLayOut({children}: {children: React.ReactNode}
+) {
+  return (
+    <>
+    <header>
+        
+    </header>
+
+        {children}
+    </>
+  )
+}
---
+++ src/app/(siteLayout)/pege.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function pege() {
+  return (
+    <main>
+      <h3 className="">constenido de la pagina</h3>
+    </main>
+  );
+}
