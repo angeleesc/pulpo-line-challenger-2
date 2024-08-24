245d47c8fa30baa11450fa9edf6e42d2c07eb27b
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-24 15:57:19 -0400

    configuracion de la tipografia montereat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
+import { Inter, Montserrat, Montserrat_Alternates } from "next/font/google";
 import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 // const
 
+// const montserrat = Montserrat_Alternates({
+//   subsets: ["latin"],
+//   variable: "--font-monserrat",
+//   weight:["100", "200", "300", "400","500", "600", "700", "800", "900"]
+// });
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-monserrat",
+  weight:["100", "200", "300", "400","500", "600", "700", "800", "900"]
 });
 
 export const metadata: Metadata = {
