c6d675135ea566ff86d5386cc7df556fdd71bc81
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-25 08:15:10 -0400

    configuracion de la ctivacion del tab

--- src/app/(siteLayout)/page.tsx
+++ src/app/(siteLayout)/page.tsx
@@ -1,11 +1,31 @@
+"use client"
+import { useState } from "react";
 import MenuTabOption from "../component/tabs/menu-tab/MenuTabOption";
+import { OptionsTabsType } from "@/types/tabOptions";
+
+
+
+
+const recargaMenuOptions: OptionsTabsType[] =[
+  {
+    value: "Recargar",
+    title: "Recargar Saldo"
+  },
+  {
+    value: "Reclamar",
+    title: "Reclamar Premio"
+  }
+]
 
 export default function Home() {
+
+  const [tabOptionSelectesState, setTabOptionSelectesState] = useState<String>("Recargar")
+
   return (
     <main className="p-8">
       <div className="ml-16">
         <h3 className="font-bold text-[25px]  mt-8">Gestion De Saldo</h3>
-        <MenuTabOption />
+        <MenuTabOption currentOption  = {tabOptionSelectesState}  setOption={setTabOptionSelectesState} options={recargaMenuOptions}  />
       </div>
     </main>
   );
--- src/app/component/tabs/menu-tab/MenuTabOption.tsx
+++ src/app/component/tabs/menu-tab/MenuTabOption.tsx
@@ -1,9 +1,28 @@
+
+"use client"
+import { OptionsTabsType } from '@/types/tabOptions'
 import React from 'react'
+import "./tab-button.css"
 
-export type TabOptionsType = string[]
 
-export default function MenuTabOption({}) {
+
+type MenuTabOptionPropsType ={
+  options:OptionsTabsType[]
+  setOption: (value:string)=> void
+  currentOption: String
+}
+
+
+export default function MenuTabOption({ options, currentOption, setOption  }:MenuTabOptionPropsType) {
   return (
-    <div>MenuTabOption</div>
+    <div>
+      {
+options.map((option, i )=>{
+  return <button key={i} onClick={()=> setOption(option.value || "Default")} className={ currentOption === option.value? "tab-button tab-button-active" : "tab-button  tab-button-disable" } >
+    {option.title}
+  </button>
+})
+      }
+    </div>
   )
 }
