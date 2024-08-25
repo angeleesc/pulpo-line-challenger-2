692da2215ba87dcac3b4dff665be0d8dd79e5bf3
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-25 07:31:04 -0400

    configuracion de manu tab options

--- src/app/(siteLayout)/page.tsx
+++ src/app/(siteLayout)/page.tsx
@@ -1,17 +1,12 @@
-import { Input } from "@material-tailwind/react";
-import Image from "next/image";
-import MaterialInput from "../component/inputs/material-inputs/MaterialInput";
+import MenuTabOption from "../component/tabs/menu-tab/MenuTabOption";
 
 export default function Home() {
   return (
     <main className="p-8">
-     <div className="ml-16">
-     <h3 className="font-bold text-[25px]  mt-8">Gestion De Saldo</h3>
-
-     {/* <Input variant="static" label="Static" placeholder="Static" /> */}
-     <MaterialInput/>
-
-     </div>
+      <div className="ml-16">
+        <h3 className="font-bold text-[25px]  mt-8">Gestion De Saldo</h3>
+        <MenuTabOption />
+      </div>
     </main>
   );
 }
