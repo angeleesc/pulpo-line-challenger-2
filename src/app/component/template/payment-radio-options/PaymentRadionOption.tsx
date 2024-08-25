c2f6894243e46f045f3effda1e09312b800138d7
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-25 13:46:26 -0400

    current paiment

--- src/app/component/template/payment-radio-options/PaymentRadionOption.tsx
+++ src/app/component/template/payment-radio-options/PaymentRadionOption.tsx
@@ -1,29 +1,45 @@
-
-
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import "./payment-radio-option.scss"
 
 export default function PaymentRadionOption() {
+  const [currenTpayment, setCurrenTpayment] = useState<string>("bizun");
 
+  console.log("current state", currenTpayment);
 
-    
-
-
-  return <div className="flex flex-col gap-8">
-    <label>
-        <input type="radio" name="payment-option" value={"credit-card"} /> 
+  return (
+    <div className="flex flex-col gap-8 payment-methos-container">
+      <label className={`payment-option-item ${currenTpayment === "credit-card" && "payment-option-item-active" } `} >
+        <input
+          type="radio"
+          name="payment-option"
+          checked={currenTpayment === "credit-card"}
+          value={"credit-card"}
+          onChange={(e) => setCurrenTpayment(e.target.value)}
+        />
         <span>Tarjeta Bancaria</span>
-    </label>
-    
-    <label>
-        <input type="radio" name="payment-option" value={"bizun"} /> 
-        <span>Bizun</span>
-    </label>
-    <label>
-        <input type="radio" name="payment-option" value={"transfer"} /> 
-        <span>Tranferencia</span>
-    </label>
-    
+      </label>
 
-    
-    </div>;
+      <label className={`payment-option-item ${currenTpayment === "bizun" && "payment-option-item-active" }`} >
+        <input
+          type="radio"
+          name="payment-option"
+          checked={currenTpayment === "bizun"}
+          value={"bizun"}
+          onChange={(e) => setCurrenTpayment(e.target.value)}
+        />
+        <span>Bizun</span>
+      </label>
+      <label className={`payment-option-item ${currenTpayment === "transfer" && "payment-option-item-active" }`}>
+        <input
+          type="radio"
+          name="payment-option"
+          checked={currenTpayment === "transfer"}
+          value={"transfer"}
+          onChange={(e) => setCurrenTpayment(e.target.value)}
+        />
+        <span>Tranferencia</span>
+      </label>
+    </div>
+  );
 }
