372c0d19ba2ec761b5b290686ddf97b1aad65875
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-25 11:57:20 -0400

    configuracion base

---
+++ src/app/component/inputs/button-group-input/ButtonGroupButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { OptionsTabsType } from "@/types/tabOptions";
+import React from "react";
+
+type MenuTabOptionPropsType = {
+  options: OptionsTabsType[];
+  setOption: (value: string) => void;
+  currentOption: String;
+};
+
+export default function ButtonGroupButton({
+  options,
+  currentOption,
+  setOption,
+}: MenuTabOptionPropsType) {
+  return (
+    <div className="flex justify-between">
+      {options.map((option, i) => {
+        return (
+          <button 
+          type="button" 
+          key={i} 
+          className={`text-center flex-grow`}
+          onClick={() => setOption(option.value || "0")}
+          >
+            {option.title && option.title}
+            {"  "} {option.icon && option.icon}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
--- src/app/component/template/rechage-balance-form/RechargeBalanceForm.tsx
+++ src/app/component/template/rechage-balance-form/RechargeBalanceForm.tsx
@@ -1,10 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import TextInput from "../../inputs/text-input/TextInput";
+import { OptionsTabsType } from "@/types/tabOptions";
+import ButtonGroupButton from "../../inputs/button-group-input/ButtonGroupButton";
+
+const moneyValuesOptions: OptionsTabsType[] = [
+  {
+    title: "5",
+    value: "5",
+    icon: <span>€</span>,
+  },
+  {
+    title: "10",
+    value: "10",
+    icon: <span>€</span>,
+  },
+  {
+    title: "20",
+    value: "20",
+    icon: <span>€</span>,
+  },
+  {
+    title: "50",
+    value: "50",
+    icon: <span>€</span>,
+  },
+  {
+    title: "100",
+    value: "100",
+    icon: <span>€</span>,
+  },
+];
 
 export default function RechargeBalanceForm() {
+  const [balanceState, setBalanceState] = useState<string>("");
 
-
+  console.log("balance a anadir", balanceState);
 
   return (
     <form action="" className="w-full mt-8">
@@ -12,8 +43,19 @@ export default function RechargeBalanceForm() {
         <h2 className="font-bold text-4xl leading-[50px]">{"10,00 €"}</h2>
         <p className="text-[15px] leading-5 font-medium">Tu saldo</p>
       </div>
-      <TextInput texttLabel={"Seleciona el importe a añadir"} placeholder="importe €" info={"importe minimo 1€"} />
-    
+      <TextInput
+        texttLabel={"Seleciona el importe a añadir"}
+        placeholder="importe €"
+        info={"importe minimo 1€"}
+        value={balanceState}
+        onChange={(e) => setBalanceState(e.target.value)}
+        type="number"
+      />
+      <ButtonGroupButton
+        currentOption={balanceState}
+        options={moneyValuesOptions}
+        setOption={setBalanceState}
+      />
     </form>
   );
 }
