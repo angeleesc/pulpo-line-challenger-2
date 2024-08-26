cccdf9594315d15183c41db53e7d34d909a64fd2
angeleesc <50447147+angeleesc@users.noreply.github.com>
2024-08-25 21:24:48 -0400

    confiraciones de mobile nav bar

--- src/app/component/Navbar/MobileNavBar.tsx
+++ src/app/component/Navbar/MobileNavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MobileNavBar() {
   return (
     <nav className="bg-primary-default w-full h-16 flex items-center px-4">
-        mobile navbar
+        <div className='flex font-bold text-white'> <h2>Gestion de saldo</h2> </div>
     </nav>
 
   )
